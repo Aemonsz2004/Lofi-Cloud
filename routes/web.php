e169bfe694e40dcde0bef26352552106f2020ac7
Aemonsz2004 <@aemonsz2004@gmail.com>
2025-03-28 20:37:22 +0800

    cleaned all of existing design and code

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,7 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+Route::get('Home', function () {
+    return Inertia::render('Home');
+})->name('Home');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
